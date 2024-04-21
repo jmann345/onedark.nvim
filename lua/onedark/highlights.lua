2dd019c2106ca2589bdf18dadc7f89aa5cc1e0ff
J* <102631389+jmann345@users.noreply.github.com>
2024-04-21 14:40:09 -0400

    Update highlights.lua

--- lua/onedark/highlights.lua
+++ lua/onedark/highlights.lua
@@ -29,22 +29,39 @@ local colors = {
 }
 
 local theme = {
-  base00 = "#1e222a",
-  base01 = "#353b45",
-  base02 = "#3e4451",
-  base03 = "#545862",
-  base04 = "#565c64",
-  base05 = "#abb2bf",
-  base06 = "#b6bdca",
-  base07 = "#c8ccd4",
-  base08 = "#e06c75",
-  base09 = "#ef596f", -- Darker version is more vibrant, switch to this
-  base0A = "#e8c88c", -- Darker version is more vibrant, switch to this
-  base0B = "#98c379",
-  base0C = "#56b6c2",
-  base0D = "#61afef",
-  base0E = "#4fa6ed", -- Darker version is more vibrant, switch to this
-  base0F = "#8b3434" -- Darker version is more vibrant, switch to this
+
+base00 = "#1e222a",
+
+base01 = "#353b45",
+
+base02 = "#3e4451",
+
+base03 = "#545862",
+
+base04 = "#565c64",
+
+base05 = "#abb2bf",
+
+base06 = "#b6bdca",
+
+base07 = "#c8ccd4",
+
+base08 = "#e06c75",
+
+base09 = "#d19a66",
+
+base0A = "#e5c07b",
+
+base0B = "#98c379",
+
+base0C = "#56b6c2",
+
+base0D = "#61afef",
+
+base0E = "#c678dd",
+
+base0F = "#be5046",
+
 }
 
 local base30 = {
